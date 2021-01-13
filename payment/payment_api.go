cd1386c751f6ffbe6f32b02d55e86fdb60e287c4
JackHunx <x@walle.online>
2021-01-13 11:12:54 +0800

    #完善jssdk

--- payment/payment_api.go
+++ payment/payment_api.go
@@ -1,8 +1,11 @@
 package payment
 
-import "github.com/gogf/gf/frame/g"
+import (
+	"github.com/gogf/gf/frame/g"
+)
 
 func (p *Payment) Certificates() {
 	response := p.getClient().RequestJson("GET", "/v3/certificates")
 	g.Dump("certs:----------------------------\n", response.ReadAllString())
+	// @todo
 }
