67d96b20ed87ba76556685a5827ae2f43b7a9adb
jackhunx <x@walle.online>
2021-01-28 23:24:05 +0800

    #添加创建试用小程序通知信息处理

--- openplatform/server/server_message.go
+++ openplatform/server/server_message.go
@@ -5,19 +5,21 @@ import (
 )
 
 const (
-	EVENT_AUTHORIZED              = 16384
-	EVENT_UNAUTHORIZED            = 32768
-	EVENT_UPDATE_AUTHORIZED       = 65536
-	EVENT_COMPONENT_VERIFY_TICKET = 131072
-	EVENT_THIRD_FAST_REGISTERED   = 262144
+	EVENT_AUTHORIZED                  = 16384
+	EVENT_UNAUTHORIZED                = 32768
+	EVENT_UPDATE_AUTHORIZED           = 65536
+	EVENT_COMPONENT_VERIFY_TICKET     = 131072
+	EVENT_THIRD_FAST_REGISTERED       = 262144
+	EVENT_THIRD_FAST_REGISTERDBETAAPP = 524288
 )
 
 var messageType = map[string]message.MessageType{
-	"authorized":                 EVENT_AUTHORIZED,
-	"unauthorized":               EVENT_UNAUTHORIZED,
-	"updateauthorized":           EVENT_UPDATE_AUTHORIZED,
-	"component_verify_ticket":    EVENT_COMPONENT_VERIFY_TICKET,
-	"notify_third_fasteregister": EVENT_THIRD_FAST_REGISTERED,
+	"authorized":                       EVENT_AUTHORIZED,
+	"unauthorized":                     EVENT_UNAUTHORIZED,
+	"updateauthorized":                 EVENT_UPDATE_AUTHORIZED,
+	"component_verify_ticket":          EVENT_COMPONENT_VERIFY_TICKET,
+	"notify_third_fasteregister":       EVENT_THIRD_FAST_REGISTERED,
+	"notify_third_fastregisterbetaapp": EVENT_THIRD_FAST_REGISTERDBETAAPP,
 }
 
 // type Message interface {
