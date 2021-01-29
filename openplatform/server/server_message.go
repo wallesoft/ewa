1fd54e100c258a3508f7bc162938ca0bc8814a9d
JackHunx <x@walle.online>
2021-01-29 16:51:09 +0800

    #更改拼写错误

--- openplatform/server/server_message.go
+++ openplatform/server/server_message.go
@@ -5,12 +5,12 @@ import (
 )
 
 const (
-	EVENT_AUTHORIZED                  = 16384
-	EVENT_UNAUTHORIZED                = 32768
-	EVENT_UPDATE_AUTHORIZED           = 65536
-	EVENT_COMPONENT_VERIFY_TICKET     = 131072
-	EVENT_THIRD_FAST_REGISTERED       = 262144
-	EVENT_THIRD_FAST_REGISTERDBETAAPP = 524288
+	EVENT_AUTHORIZED                 = 16384
+	EVENT_UNAUTHORIZED               = 32768
+	EVENT_UPDATE_AUTHORIZED          = 65536
+	EVENT_COMPONENT_VERIFY_TICKET    = 131072
+	EVENT_THIRD_FAST_REGISTER        = 262144
+	EVENT_THIRD_FAST_REGISTERBETAAPP = 524288
 )
 
 var messageType = map[string]message.MessageType{
@@ -18,6 +18,6 @@ var messageType = map[string]message.MessageType{
 	"unauthorized":                     EVENT_UNAUTHORIZED,
 	"updateauthorized":                 EVENT_UPDATE_AUTHORIZED,
 	"component_verify_ticket":          EVENT_COMPONENT_VERIFY_TICKET,
-	"notify_third_fasteregister":       EVENT_THIRD_FAST_REGISTERED,
-	"notify_third_fastregisterbetaapp": EVENT_THIRD_FAST_REGISTERDBETAAPP,
+	"notify_third_fasteregister":       EVENT_THIRD_FAST_REGISTER,
+	"notify_third_fastregisterbetaapp": EVENT_THIRD_FAST_REGISTERBETAAPP,
 }
