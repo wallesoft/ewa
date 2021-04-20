cbc1109d56f2a604dcac9ab83c1386a8c72c5315
jackhunx <x@walle.online>
2021-04-20 23:51:24 +0800

    #添加小程序模板相关操作

--- openplatform/openplatform_weapp.go
+++ openplatform/openplatform_weapp.go
@@ -1,9 +1,24 @@
 package openplatform
 
-import "github.com/gogf/gf/encoding/gjson"
+import (
+	"github.com/gogf/gf/encoding/gjson"
+	"github.com/gogf/gf/frame/g"
+)
 
 //获取代码草稿列表 @see https://developers.weixin.qq.com/doc/oplatform/Third-party_Platforms/Mini_Programs/code_template/gettemplatedraftlist.html
 func (op *OpenPlatform) GetTemplateDraftList() *gjson.Json {
 	client := op.getClientWithToken()
 	return client.RequestJson("GET", "wxa/gettemplatedraftlist")
+}
+
+//将草稿添加到模板库 @see https://developers.weixin.qq.com/doc/oplatform/Third-party_Platforms/Mini_Programs/code_template/addtotemplate.html
+func (op *OpenPlatform) AddToTemplate(draftId string) *gjson.Json {
+	client := op.getClientWithToken()
+	return client.RequestJson("POST", "wxa/addtotemplate", g.Map{"draft_id": draftId})
+}
+
+//获取模板库列表 @see https://developers.weixin.qq.com/doc/oplatform/Third-party_Platforms/Mini_Programs/code_template/gettemplatelist.html
+func (op *OpenPlatform) GetTemplateList() *gjson.Json {
+	client := op.getClientWithToken()
+	return client.RequestJson("GET", "wxa/gettemplatelist")
 }
