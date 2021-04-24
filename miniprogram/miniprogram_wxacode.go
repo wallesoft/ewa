d3c25e095e9c4cdde4e5ea9bc9cbc3c8d3a845d1
jackhunx <x@walle.online>
2021-04-24 17:17:06 +0800

    #添加第三方平台小程序上传代码，获取体验码接口

--- miniprogram/miniprogram_wxacode.go
+++ miniprogram/miniprogram_wxacode.go
@@ -11,8 +11,8 @@ import (
 
 //小程序码
 type AppCode struct {
-	mp  *MiniProgram
-	raw []byte
+	Mp  *MiniProgram
+	Raw []byte
 }
 
 //错误
@@ -24,7 +24,7 @@ type AppCodeError struct {
 //AppCode
 func (mp *MiniProgram) AppCode() *AppCode {
 	return &AppCode{
-		mp: mp,
+		Mp: mp,
 	}
 }
 
@@ -36,8 +36,8 @@ func (c *AppCode) Save(path string, name ...string) (string, *AppCodeError) {
 	} else {
 		codeName = guid.S() + ".png"
 	}
-	if gjson.Valid(c.raw) {
-		err := gjson.New(c.raw)
+	if gjson.Valid(c.Raw) {
+		err := gjson.New(c.Raw)
 		if err.GetInt("errcode") != 0 {
 			return "", &AppCodeError{
 				ErrCode: err.GetInt("errcode"),
@@ -45,7 +45,7 @@ func (c *AppCode) Save(path string, name ...string) (string, *AppCodeError) {
 			}
 		}
 	}
-	err := gfile.PutBytes(fmt.Sprintf("%s/%s", path, codeName), c.raw)
+	err := gfile.PutBytes(fmt.Sprintf("%s/%s", path, codeName), c.Raw)
 	if err != nil {
 		return "", &AppCodeError{
 			ErrCode: -1,
@@ -63,31 +63,31 @@ func (c *AppCode) CreateQrCode(path string, width ...int) *AppCode {
 	if len(width) > 0 {
 		param["width"] = width[0]
 	}
-	client := c.mp.GetClientWithToken()
-	c.raw = client.RequestRaw("POST", "cgi-bin/wxaapp/createwxaqrcode", param)
+	client := c.Mp.GetClientWithToken()
+	c.Raw = client.RequestRaw("POST", "cgi-bin/wxaapp/createwxaqrcode", param)
 	return c
 }
 
 //获取小程序码 有数量限制，详细查看@see https://developers.weixin.qq.com/miniprogram/dev/api-backend/open-api/qr-code/wxacode.get.html
 func (c *AppCode) Get(path string, config ...g.Map) *AppCode {
-	client := c.mp.GetClientWithToken()
+	client := c.Mp.GetClientWithToken()
 	param := make(g.Map)
 	if len(config) > 0 {
 		param = config[0]
 	}
 	param["path"] = path
-	c.raw = client.RequestRaw("POST", "wxa/getwxacode", param)
+	c.Raw = client.RequestRaw("POST", "wxa/getwxacode", param)
 	return c
 }
 
 //获取小程序码 不限制数量
 func (c *AppCode) GetUnlimit(scene string, config ...g.Map) *AppCode {
-	client := c.mp.GetClientWithToken()
+	client := c.Mp.GetClientWithToken()
 	param := make(g.Map)
 	if len(config) > 0 {
 		param = config[0]
 	}
 	param["scene"] = scene
-	c.raw = client.RequestRaw("POST", "wxa/getwxacodeunlimit", param)
+	c.Raw = client.RequestRaw("POST", "wxa/getwxacodeunlimit", param)
 	return c
 }
--- openplatform/miniprogram_code.go
+++ openplatform/miniprogram_code.go
@@ -2,10 +2,12 @@ package openplatform
 
 import (
 	"gitee.com/wallesoft/ewa/kernel/http"
+	"gitee.com/wallesoft/ewa/miniprogram"
 	"github.com/gogf/gf/frame/g"
 )
 
-//代码上传
+//代码上传 @see https://developers.weixin.qq.com/doc/oplatform/Third-party_Platforms/Mini_Programs/code/commit.html
+//and ext_json @see https://developers.weixin.qq.com/miniprogram/dev/devtools/ext.html#%E5%B0%8F%E7%A8%8B%E5%BA%8F%E6%A8%A1%E6%9D%BF%E5%BC%80%E5%8F%91
 func (mp *MiniProgram) Commit(templateId string, extJson string, version string, desc string) *http.ResponseData {
 	client := mp.GetClientWithToken()
 	return &http.ResponseData{
@@ -15,5 +17,22 @@ func (mp *MiniProgram) Commit(templateId string, extJson string, version string,
 			"user_version": version,
 			"user_desc":    desc,
 		}),
+	}
+}
+
+//获取已上传的代码的页面列表 @see https://developers.weixin.qq.com/doc/oplatform/Third-party_Platforms/Mini_Programs/code/get_page.html
+func (mp *MiniProgram) GetPage() *http.ResponseData {
+	client := mp.GetClientWithToken()
+	return &http.ResponseData{
+		Json: client.RequestJson("GET", "wxa/get_page"),
+	}
+}
+
+//获取体验二维码 @see https://developers.weixin.qq.com/doc/oplatform/Third-party_Platforms/Mini_Programs/code/get_qrcode.html
+func (mp *MiniProgram) GetQrcode(path string) *miniprogram.AppCode {
+	client := mp.GetClientWithToken()
+	return &miniprogram.AppCode{
+		Mp:  mp.MiniProgram,
+		Raw: client.RequestRaw("GET", "wxa/get_qrcode", g.Map{"path": path}),
 	}
 }
