859eadedb3e9126891d0d62b904b036a2df19c27
jackhunx <x@walle.online>
2021-04-26 16:57:20 +0800

    通过静默授权的方法拿到appid https://gitee.com/vbit/dashboard?issue_id=I2CKAR

--- miniprogram/miniprogram_security.go
+++ miniprogram/miniprogram_security.go
@@ -5,7 +5,7 @@ import (
 	"github.com/gogf/gf/frame/g"
 )
 
-//CheckText
+//CheckText @see https://developers.weixin.qq.com/miniprogram/dev/api-backend/open-api/sec-check/security.msgSecCheck.html
 func (mp *MiniProgram) CheckText(content string) *http.ResponseData {
 	return &http.ResponseData{
 		Json: mp.GetClientWithToken().RequestJson("POST", "wxa/msg_sec_check", g.Map{"content": content}),
