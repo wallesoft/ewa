0bcf7538787a6c1170c82123ee13cc98b7eb35ad
jackhunx <x@walle.online>
2023-04-18 17:16:05 +0800

    change oauth

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.14
 
 require (
 	gitee.com/wallesoft/go v1.0.2
+	gitee.com/wallesoft/oauth2 v0.0.0-20230417034752-e4111caef241 // indirect
 	github.com/gogf/gf/v2 v2.3.3
 )
--- officialaccount/officialaccount_oauth.go
+++ officialaccount/officialaccount_oauth.go
@@ -1,14 +1,21 @@
 package officialaccount
 
 import (
-	"gitee.com/wallesoft/go/oauth2"
+	"context"
+
+	"gitee.com/wallesoft/oauth2"
+	"gitee.com/wallesoft/oauth2/client/wechat"
 )
 
-//OAuth
-//@see https://gitee.com/wallesoft/go/oauth2
-func (oa *OfficialAccount) OAuth() *oauth2.WechatConfig {
-	return oauth2.Wechat(oauth2.Config{
-		ClientID:     oa.config.AppID,
-		ClientSecret: oa.config.Secret,
-	})
+// OAuth
+// @see https://gitee.com/wallesoft/go/oauth2
+func (oa *OfficialAccount) OAuth(ctx context.Context, config *wechat.Config) *oauth2.Oauth {
+	if config.ClientID == "" {
+		config.ClientID = oa.config.AppID
+	}
+	if config.ClientSecret == "" {
+		config.ClientSecret = oa.config.Secret
+	}
+
+	return oauth2.New(ctx, config)
 }
