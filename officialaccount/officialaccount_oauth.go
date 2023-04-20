a51c07e6b4f6dda484b198fb0a1c24335e1a27ae
jackhunx <x@walle.online>
2023-04-20 17:29:33 +0800

    change oauth to wallesoft/oauth2

--- officialaccount/officialaccount_oauth.go
+++ officialaccount/officialaccount_oauth.go
@@ -10,11 +10,11 @@ import (
 // OAuth
 // @see https://gitee.com/wallesoft/go/oauth2
 func (oa *OfficialAccount) OAuth(ctx context.Context, config *wechat.Config) *oauth2.Oauth {
-	if config.ClientID == "" {
-		config.ClientID = oa.config.AppID
+	if config.AppID == "" {
+		config.AppID = oa.config.AppID
 	}
-	if config.ClientSecret == "" {
-		config.ClientSecret = oa.config.Secret
+	if config.Secret == "" {
+		config.Secret = oa.config.Secret
 	}
 
 	return oauth2.New(ctx, config)
