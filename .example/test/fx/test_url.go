1a981a7101f6dc08a8d2fdbb589b51a1366cc8d2
jackhunx <x@walle.online>
2024-04-02 14:57:03 +0800

    test

--- .example/test/fx/test_url.go
+++ .example/test/fx/test_url.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	var url = &url.Values{}
-	url.Add("test", "test")
+	url.Add("test", "test1")
 	url.Add("test", "test2")
 	fmt.Println(fmt.Sprintf("ecode:%s", url.Encode()))
 }
